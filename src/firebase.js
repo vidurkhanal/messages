5dec61cd44840c46168638de6c02101186eab00a
Vidur <vidurkhanal@outlook.com>
2020-09-21 09:11:48 +0545

    Initial React Setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCr3y1gAwslVgFc03JkrNbeTlV36pPt18s",
+  authDomain: "vidsociaal.firebaseapp.com",
+  databaseURL: "https://vidsociaal.firebaseio.com",
+  projectId: "vidsociaal",
+  storageBucket: "vidsociaal.appspot.com",
+  messagingSenderId: "679817015482",
+  appId: "1:679817015482:web:3ed152a7578931d2d09283",
+};
